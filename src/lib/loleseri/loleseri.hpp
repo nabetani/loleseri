97faea3a633c8bfc73ad63357667903a031f8e47
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-07-27 10:06:50 +0900

    エンディアン対応

--- src/lib/loleseri/loleseri.hpp
+++ src/lib/loleseri/loleseri.hpp
@@ -1,25 +1,26 @@
 #pragma once
 
+#include <array>
 #include <iterator>
+#include <loleseri/endian.hpp>
 #include <tuple>
 #include <type_traits>
-#include <array>
 
 namespace loleseri {
 
 template <typename target_type, int typecat> struct serializer_impl;
 
-template< typename target_type >
-struct type_category{
-  enum{
-    value = ( typename std::is_same<bool, target_type>::type() ? 1 : 0)
+template <typename target_type> struct type_category {
+  enum {
+    value = (typename std::is_same<bool, target_type>::type() ? 1 : 0) +
+            (typename std::is_arithmetic<target_type>::type() ? 2 : 0)
   };
 };
 
-namespace tcat{
-  constexpr int boolean = type_category<bool>::value;
-  constexpr int arithmetic = type_category<int>::value;
-}
+namespace tcat {
+constexpr int boolean = type_category<bool>::value;
+constexpr int arithmetic = type_category<int>::value;
+} // namespace tcat
 
 template <typename target_type>
 using serializer =
@@ -27,30 +28,28 @@ using serializer =
 } // namespace loleseri
 
 template <typename target_type_>
-struct loleseri::serializer_impl<target_type_, loleseri::tcat::arithmetic>
-{
+struct loleseri::serializer_impl<target_type_, loleseri::tcat::arithmetic> {
   using target_type = target_type_;
   using buffer = std::array<std::uint8_t, sizeof(target_type)>;
 
-  template< typename itor_t >
-  static
-  itor_t serialize( itor_t begin, itor_t end, target_type const * obj )
-  {
-    auto p = reinterpret_cast<std::uint8_t const*>(obj);
-    return std::copy(p, p+sizeof(target_type), begin );
+  template <typename itor_t>
+  static itor_t serialize(itor_t begin, itor_t end, target_type const *obj) {
+#if LOLESERI_LITTLE_ENDIAN
+    auto p = reinterpret_cast<std::uint8_t const *>(obj);
+    return std::copy(p, p + sizeof(target_type), begin);
+#else
+#error "you should write something here."
+#endif
   }
 };
 
 template <typename target_type_>
-struct loleseri::serializer_impl<target_type_, loleseri::tcat::boolean>
-{
+struct loleseri::serializer_impl<target_type_, loleseri::tcat::boolean> {
   using target_type = target_type_;
   using buffer = std::array<std::uint8_t, sizeof(target_type)>;
 
-  template< typename itor_t >
-  static
-  itor_t serialize( itor_t begin, itor_t end, target_type const * obj )
-  {
+  template <typename itor_t>
+  static itor_t serialize(itor_t begin, itor_t end, target_type const *obj) {
     *begin = *obj ? 1 : 0;
     return std::next(begin);
   }

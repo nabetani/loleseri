86278c3c1c1d881cbcab359497d28fd73327807f
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-07-27 18:18:09 +0900

    利用例その1を整備

--- src/examples/struct/main.cpp
+++ src/examples/struct/main.cpp
@@ -3,30 +3,75 @@
 #include <iostream>
 #include <tuple>
 
+#include <iomanip>
+#include <ios>
 #include <loleseri/loleseri.hpp>
 
-struct Foo {
-  std::uint8_t hoge_;
-  std::int32_t fuga_;
-  float piyo_;
+/** 様々な算術型を持つ構造体 */
+struct SimpleStruct {
+  std::int8_t foo;
+  std::int16_t bar;
+  std::int32_t baz;
+  std::int64_t qux;
+  float quux;
+  double corge;
+  bool grault;
 };
 
-namespace loleseri {
-template <> struct items<Foo> {
-  using list_type = std::tuple<
-    std::uint8_t Foo::*,
-    std::int32_t Foo::*,
-    float Foo::*>;
-  static inline list_type
-  list(){
-      return {&Foo::hoge_, &Foo::fuga_, &Foo::piyo_};
+std::ostream &operator<<(std::ostream &o, SimpleStruct const &s) {
+  return o                                 //
+         << static_cast<int>(s.foo) << " " //
+         << s.bar << " "                   //
+         << s.baz << " "                   //
+         << s.qux << " "                   //
+         << s.quux << " "                  //
+         << s.corge << " "                 //
+         << std::boolalpha << s.grault;
+}
+
+/** SimpleStructのメンバリスト */
+auto const simpleStructMembers =
+    std::make_tuple(&SimpleStruct::foo, &SimpleStruct::bar, &SimpleStruct::baz,
+                    &SimpleStruct::qux, &SimpleStruct::quux,
+                    &SimpleStruct::corge, &SimpleStruct::grault);
+
+/** シリアライザから見えるようにする */
+template <> struct loleseri::items<SimpleStruct> {
+  static decltype(simpleStructMembers) list() { return simpleStructMembers; }
+};
+
+template <typename container> void dump(container const &c) {
+  using namespace std;
+  for (auto const &e : c) {
+    cout << setw(2) << setfill('0') << hex << static_cast<int>(e) << " ";
   }
-};
-} // namespace loleseri
+  cout << endl;
+}
 
-int main()
-{
-  std::array<std::uint8_t, loleseri::size<Foo>()> buffer;
-  Foo foo;
-  loleseri::serialize( buffer.begin(), buffer.end(), &foo );
+int main() {
+  SimpleStruct hoge{1, 2, 3, 4, 5, 6, true};
+
+  // シリアライザクラス
+  using seri = loleseri::serializer<SimpleStruct>;
+
+  // 必要なサイズを計算
+  constexpr size_t size = seri::size;
+
+  // バッファを確保
+  std::array<std::uint8_t, size> buffer;
+
+  // シリアライズ
+  seri::serialize(buffer.begin(), buffer.end(), &hoge);
+
+  // シリアライズ結果をダンプ
+  dump(buffer);
+
+  // デシリアライザクラス
+  using deseri = loleseri::deserializer<SimpleStruct>;
+
+  // 復元
+  auto fuga = deseri::deserialize(buffer.cbegin(), buffer.cend());
+
+  std::cout << "hoge: " << hoge << "\n"
+            << "fuga: " << fuga << std::endl;
 }
